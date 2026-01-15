bf06fbd345dfbce319f5a0eaa8acb42f10b603c3
koalacodee <koalacodee@gmail.com>
2026-01-15 17:15:46 +0200

    refactor(drizzle-migration): enhance index and column management in SQL migration
    - Updated DROP INDEX and CREATE INDEX statements to use "IF EXISTS" and "IF NOT EXISTS" for safer execution.
    - Modified ALTER TABLE commands to include "IF EXISTS" for dropping columns, improving migration robustness and preventing errors during execution.

--- src/common/drizzle/0001_pink_shiver_man.sql
+++ src/common/drizzle/0001_pink_shiver_man.sql
@@ -1,6 +1,6 @@
-DROP INDEX "idx_support_tickets_subject_lower";--> statement-breakpoint
+DROP INDEX IF EXISTS "idx_support_tickets_subject_lower";--> statement-breakpoint
 ALTER TABLE "supervisors" ALTER COLUMN "permissions" SET DATA TYPE "public"."AdminPermissions"[] USING "permissions"::"public"."AdminPermissions"[];--> statement-breakpoint
-CREATE INDEX "idx_support_tickets_subject_lower" ON "support_tickets" USING btree (lower(TRIM(BOTH FROM subject)));--> statement-breakpoint
-ALTER TABLE "profile_pictures" DROP COLUMN "original_name";--> statement-breakpoint
-ALTER TABLE "profile_pictures" DROP COLUMN "mime_type";--> statement-breakpoint
-ALTER TABLE "profile_pictures" DROP COLUMN "size";
+CREATE INDEX IF NOT EXISTS "idx_support_tickets_subject_lower" ON "support_tickets" USING btree (lower(TRIM(BOTH FROM subject)));--> statement-breakpoint
+ALTER TABLE "profile_pictures" DROP COLUMN IF EXISTS "original_name";--> statement-breakpoint
+ALTER TABLE "profile_pictures" DROP COLUMN IF EXISTS "mime_type";--> statement-breakpoint
+ALTER TABLE "profile_pictures" DROP COLUMN IF EXISTS "size";
