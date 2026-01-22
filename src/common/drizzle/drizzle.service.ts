63bbe2373eeaab98f19b87f2bc7405e385f4ce21
koalacodee <koalacodee@gmail.com>
2026-01-22 17:09:20 +0200

    refactor(drizzle.service): integrate relations into schema for enhanced database management
    - Combined schema and relations into a single fullSchema object for improved organization and clarity.
    - Updated drizzleClient initialization and client getter to utilize the new fullSchema, ensuring consistent database interactions.

--- src/common/drizzle/drizzle.service.ts
+++ src/common/drizzle/drizzle.service.ts
@@ -2,12 +2,15 @@ import { ConfigService } from '@nestjs/config';
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
+import * as relations from './relations';
 import { Injectable } from '@nestjs/common';
+
+const fullSchema = { ...schema, ...relations };
 
 @Injectable()
 export class DrizzleService {
   private pgClient: Pool;
-  private drizzleClient: NodePgDatabase<typeof schema>;
+  private drizzleClient: NodePgDatabase<typeof fullSchema>;
 
   constructor(private readonly config: ConfigService) {
     this.pgClient = new Pool({
@@ -15,7 +18,7 @@ export class DrizzleService {
     });
     this.drizzleClient = drizzle({
       client: this.pgClient,
-      schema,
+      schema: fullSchema,
     });
   }
 
@@ -23,7 +26,7 @@ export class DrizzleService {
     return this.pgClient;
   }
 
-  get client(): NodePgDatabase<typeof schema> {
+  get client(): NodePgDatabase<typeof fullSchema> {
     return this.drizzleClient;
   }
 }
